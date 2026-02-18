f4d420d6b4f574829a8a0ed517dd9b1ce3f3628a
Deng Kuat <denq1738@gmail.com>
2026-02-18 15:21:58 +0200

    Wealther Component[button to fetch, tempearature ]

---
+++ weather-app/src/components/weatherInfomation.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+
+export default function WeatherInfomation() {
+  
+
+  return (
+    <div>
+      <button className='mb-8 border py-2 px-6 rounded-md hover:bg-slate-900 duration-150'>Get Weather</button>
+      <h2 className=' text-3xl font-semibold'>Temperature:</h2>
+    </div>
+  )
+}
