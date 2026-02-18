8f5c4b51a96c5e5ee5585ea4a14f31e8285559df
Deng Kuat <denq1738@gmail.com>
2026-02-18 15:42:43 +0200

    Weather App(Fetching temperature and display on click button)

--- weather-app/src/components/weatherInfomation.jsx
+++ weather-app/src/components/weatherInfomation.jsx
@@ -1,12 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react';
 
-export default function WeatherInfomation() {
-  
+export default function WeatherInformation() {
+  const [temperature, setTemperature] = useState(null);
+
+  const fetchTemperature = async () => {
+    try {
+      const response = await fetch(
+        "https://api.open-meteo.com/v1/forecast?latitude=-1.95&longitude=30.06&current_weather=true"
+      );
+
+      const data = await response.json();
+      setTemperature(data.current_weather.temperature);
+    } catch (err) {
+      console.error('Failed to fetch temperature', err);
+    }
+  };
 
   return (
     <div>
-      <button className='mb-8 border py-2 px-6 rounded-md hover:bg-slate-900 duration-150'>Get Weather</button>
-      <h2 className=' text-3xl font-semibold'>Temperature:</h2>
+      <button
+        className='mb-8 border py-2 px-6 rounded-md hover:bg-slate-900 duration-150'
+        onClick={fetchTemperature}
+      >
+        Get Weather
+      </button>
+
+      {temperature !== null && <p>Temperature: {temperature}°C</p>}
     </div>
-  )
+  );
 }
